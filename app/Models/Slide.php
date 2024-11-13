927b6dc4f9f428649e807bc4463cef97000a3eda
AminEtemadifar <aminetemadifar.w@gmail.com>
2024-11-13 21:43:09 +0330

    initial commit

---
+++ app/Models/Slide.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Slide extends Model
+{
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Fit::Contain, 300, 300)
+            ->nonQueued();
+    }
+
+}
