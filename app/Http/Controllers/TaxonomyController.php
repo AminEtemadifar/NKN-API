bb25bb967c31cc7ce6b0ca037ef7fc42628b74f4
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-02-08 23:23:27 +0330

    index taxonomies

---
+++ app/Http/Controllers/TaxonomyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaxonomyResource;
+use App\Models\Taxonomy;
+use Illuminate\Http\Request;
+
+class TaxonomyController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/taxonomies",
+     *     summary="Get list of taxonomies",
+     *     tags={"Taxonomies"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/TaxonomyResource")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     * )
+     */    public function index()
+    {
+        return TaxonomyResource::collection(Taxonomy::all());
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Taxonomy $taxonomy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Taxonomy $taxonomy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Taxonomy $taxonomy)
+    {
+        //
+    }
+}
