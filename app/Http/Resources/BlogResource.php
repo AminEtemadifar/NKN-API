a54e131a3a68efc6bbfd76824b101939b7a3ae1d
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-02-10 21:34:14 +0330

    blogresource

--- app/Http/Resources/BlogResource.php
+++ app/Http/Resources/BlogResource.php
@@ -7,10 +7,44 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BlogResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
+    /** @OA\Schema(
+     *     schema="BlogResource",
+     *     type="object",
+     *     title="Blog Resource",
+     *     description="Blog resource representation",
+     *     @OA\Property(
+     *         property="title",
+     *         type="string",
+     *         description="The title of the blog"
+     *     ),
+     *     @OA\Property(
+     *         property="sub_title",
+     *         type="string",
+     *         description="The subtitle of the blog"
+     *     ),
+     *     @OA\Property(
+     *         property="duration",
+     *         type="integer",
+     *         description="The duration of the blog"
+     *     ),
+     *     @OA\Property(
+     *         property="created_at",
+     *         type="string",
+     *         format="date-time",
+     *         description="The creation date of the blog"
+     *     ),
+     *     @OA\Property(
+     *         property="description",
+     *         type="string",
+     *         description="The description of the blog"
+     *     ),
+     *     @OA\Property(
+     *         property="doctor",
+     *         type="object",
+     *         description="The doctor associated with the blog",
+     *         ref="#/components/schemas/DoctorResource"
+     *     )
+     * )
      */
     public function toArray(Request $request): array
     {
@@ -20,7 +54,7 @@ class BlogResource extends JsonResource
             'duration' => $this->duration,
             'created_at' => $this->created_at,
             'description' => $this->description,
-            'doctor' => $this->whenLoaded('doctor' , function (){
+            'doctor' => $this->whenLoaded('doctor', function () {
                 return new DoctorResource($this->doctor);
             })
         ];
